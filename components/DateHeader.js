ff0d7730603c33818c066407719e3a0804c4b1cc
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-10-02 16:18:21 -0700

    - update AddEntry.js  - add DateHeader.js  - add UdaciSlider.js  - add UdaciStepper.js

---
+++ components/DateHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function DateHeader({date}) {
+  return (
+      <Text>
+        {date}
+      </Text>
+  )
+}
